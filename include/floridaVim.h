6e762c1e4aaeccbb159f61a09ae4ee7a66aeda52
henryRiveraCS <44453113+henryriveraCS@users.noreply.github.com>
2021-07-17 09:35:25 -0400

    updated floridaVim, does nothing yet

--- include/floridaVim.h
+++ include/floridaVim.h
@@ -1,6 +1,32 @@
+#ifndef FLORIDAVIM_H
+#define FLORIDAVIM_H
 #include <stdio.h>
 
-void launchVim(char* cmd)
+int launchFloridaVim(char *dir, char *FileName);
+
+struct editorDisplay
 {
-	printf("VIM LAUNCHED");
-}
+	int screenRows;
+	int screenCols;
+	int currentRow;
+	int currentCol;
+	char *fileName;
+
+};
+
+struct editorKey
+{
+	//available movements
+	char up;
+	char down;
+	char left;
+	char right;
+	//this prints the current status of file ("editing:", "menu:", etc)
+	char statusMsg[50];
+
+	//tracks current mouse position
+	int currentCol;
+	int currentRow;
+};
+
+#endif /* FLORIDAVIM_H */
