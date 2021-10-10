a6dfef2f7a7096a185997e532f3a508185ef7ffd
Henry <henryrivera.cs@gmail.com>
2021-10-10 14:07:07 -0400

    netIO now included

---
+++ include/netIO.h
@@ -0,0 +1,11 @@
+#ifndef NETIO_H
+#define NETIO_H
+
+void printIP(char *ip, int len);
+void convertCharToIP(char *ip, int len, int *ipArr[4]);
+//supports ipv4 for now
+void ping (char *ip, int ipLength);
+char *sendMsg (char *msg);
+int wget (char *url);
+
+#endif /* NETIO_H */
