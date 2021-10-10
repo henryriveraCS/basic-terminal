fe7f6a092e6ed780584bda7d9d92136675907264
Henry <henryrivera.cs@gmail.com>
2021-10-09 20:52:10 -0400

    fixed type

--- include/fileIO.h
+++ include/fileIO.h
@@ -3,7 +3,7 @@
 
 #include <stdbool.h>
 
-bool isFile(const char *filePaht);
+bool isFile(const char *filePath);
 void removeFile(const char *dir, const char *fileName);
 void createFile(const char *dir, const char *fileName);
 void copyFile(const char *dir, const char *oldFileName, const char *newFileName);
