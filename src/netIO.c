0e48531924ac75984e024d1e2918de4f67b495b2
Henry <henryrivera.cs@gmail.com>
2021-10-12 22:12:52 -0400

    removed unused res var

--- src/netIO.c
+++ src/netIO.c
@@ -48,12 +48,12 @@ void printURL(char *url)
 	CURL *curl = curl_easy_init();
 	if(curl)
 	{
-		CURLcode res;
+		//CURLcode res; // used for reading codes
 		//start reading the URL
 		curl_easy_setopt(curl, CURLOPT_URL, url);
 		//curl_easy_setopt(curl, CURLOPT_PROTOCOLS, CURLPROTO_HTTP);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);
-		res = curl_easy_perform(curl);
+		//res = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
 	}
 	else
